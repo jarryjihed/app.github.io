7e026e8d021b0e6583ac3cfe69c04530236114be
jarryjihed <105054552+jarryjihed@users.noreply.github.com>
2022-05-06 13:59:15 +0200

    commit1
    1er commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Veuillez saisir votre nom.');
+  localStorage.setItem('nom', myName);
+  myHeading.textContent = 'Bienvenue, ' + myName;
+}
+
+if (!localStorage.getItem('nom')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('nom');
+  myHeading.textContent = 'Bienvenue, ' + storedName;
+}
+
+myButton.addEventListener('click', function() {
+  setUserName();
+});
